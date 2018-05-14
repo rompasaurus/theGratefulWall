71047f8ad22c81c7dec9676b545d88cbc55e3137
EC2 Default User <ec2-user@ip-172-31-11-9.us-east-2.compute.internal>
2018-05-14 07:25:27 +0000

    first tgw commit v1 test framework

---
+++ app.js
@@ -0,0 +1,77 @@
+//import and initialize npm frameworks 
+//Body parser allows for the parsing of crud commands pulling the body data of the url requested
+var bodyParser  = require("body-parser"),
+//mongoose interfaces with mongoDB and allows for schema creation and db manipulation
+    mongoose    = require("mongoose"),
+//express handles the http server and requests via easy to use notation
+    express     = require("express"),
+//app initilizes the express frameworks and is the variable in which all express commands are acted upon
+    app         = express();
+//Connect to DB (DB exists locally for now) named the_grateful_wall_gratitudes if db non-existant it will be created
+mongoose.connect("mongodb://localhost/the_grateful_wall_gratitudes");
+//establishes ejs as the primary format the will be used to present web data allowing the .ejs to be excluded when rendering
+app.set("view engine", "ejs");
+//establishes the public folder as a root folder this will contain css stysheets
+app.use(express.static("public"));
+//Boiler initializaes formats for bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//DB SCHEMA and initialization
+var gratitudeSchema = new mongoose.Schema({
+    description: String,
+    name: String,
+    created: {type: Date, default: Date.now},
+    lastVote: {type: Date, default: Date.now},
+    upvote: {type: Number, default: 1},
+    downvote: {type: Number, default: 1},
+    comments: [String]
+});
+var Gratitude = mongoose.model("Gratitude",gratitudeSchema);
+
+//Test Data adding with error catching
+// Gratitude.create(
+//     {
+//     description:"wet shits",
+//     comments: "This post blows ass, go die",
+//     name: "Dick Buttlicker"
+//     }, function(err, gratitude){
+//         if(err){
+//             console.log("I cant add this shit something is wrong");
+//         }else{
+//             console.log("new gratitude added");
+//             console.log(gratitude);
+//         }
+//     })
+
+//presents the landing page for the root of the site
+app.get("/new", function (req, res) {
+    res.render("new");
+});
+
+//Adds gratitude submitted via new form page 
+app.post("/", function(req,res){
+    Gratitude.create(req.body.gratitude, function(err,newGratitude){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/");
+        }
+    })
+})
+
+app.get('/',function(req,res){
+    Gratitude.find({}, function(err,gratitudes){
+        if(err){
+            console.log("we aint found shit in no db");
+        }else{
+        res.render('landing',{gratitudes:gratitudes});
+        }
+    })
+});
+
+
+
+//start http server and listen on c9 defaul ip and port
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Grateful wall has started and is listening");
+});
