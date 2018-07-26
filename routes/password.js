d3466fe6ae251e28f378fa7c0d14c13541cfa1e8
Michael <michael.e.mchenry@gmail.com>
2018-07-26 05:52:07 -0400

    added password reset capabilities

--- routes/password.js
+++ routes/password.js
@@ -1,10 +1,10 @@
-const express    = require("express"),
-    router     = express.Router(),
-    User       = require("../models/user"),
-    async      = require("async"),
-    nodemailer = require("nodemailer"),
-    crypto     = require("crypto");
-
+const   express    = require("express"),
+        router     = express.Router(),
+        User       = require("../models/user"),
+        async      = require("async"),
+        nodemailer = require("nodemailer"),
+        crypto     = require("crypto");
+        flash      = require("connect-flash");
 // forgot password
 router.get("/password_reset", (req, res) => res.render("password_reset"))
 
