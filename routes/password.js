b8b65595126c806a43f14a958c7c4810a8e9bb02
Rompasaurus <michael.e.mchenry@gmail.com>
2018-07-30 02:08:39 -0400

    added password reset capabilities, and form link

--- routes/password.js
+++ routes/password.js
@@ -24,7 +24,7 @@ router.post("/password_reset", (req, res, next) => {
         User.findOne({ email: req.body.email }, (err, user) => {
             if (err) throw err;
         if (!user) {
-            req.flash("error", "That YelpCamp account doesn't exist.");
+            req.flash("error", "That Grateful wall account doesn't exist.");
             return res.redirect("/password_reset");
         }
 
@@ -32,7 +32,7 @@ router.post("/password_reset", (req, res, next) => {
         user.resetPasswordExpires = Date.now() + 3600000; // ms, 1hour
 
         user.save(err => done(err, token, user));
-    });
+        });
     },
     function(token, user, done) {
         // indicate email account and the content of the confirmation letter
@@ -51,7 +51,7 @@ router.post("/password_reset", (req, res, next) => {
             "We've received a request to reset your password. If you didn't make the request, just ignore this email. Otherwise, you can reset your password using this link:\n\n" +
             "http://" + req.headers.host + "/reset/" + token + "\n\n" +
             "Thanks.\n"+
-            "The YelpCamp Team\n"
+            "The Grateful wall Team\n"
         };
         // send the email
         smtpTransport.sendMail(mailOptions, err => {
@@ -61,21 +61,21 @@ router.post("/password_reset", (req, res, next) => {
         done(err, "done");
     });
     }
-], err => {
+    ], err => {
     if (err) return next(err);
-res.redirect("/password_reset");
-});
+    res.redirect("/password_reset");
+    });
 });
 
 // reset password ($gt -> selects those documents where the value is greater than)
 router.get("/reset/:token", (req, res) => {
     User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
     if (err) throw err;
-if (!user) {
-    req.flash("error", "Password reset token is invalid or has expired.");
-    res.redirect("/password_reset");
-} else { res.render("reset", { token: req.params.token }) }
-});
+    if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        res.redirect("/password_reset");
+    }else { res.render("reset", { token: req.params.token }) }
+    });
 });
 
 // update password
@@ -84,30 +84,30 @@ router.post("/reset/:token", (req, res) => {
     function(done) {
         User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
             if (err) throw err;
-        if (!user) {
-            req.flash("error", "Password reset token is invalid or has expired.");
-            return res.redirect("/password_reset");
-        }
-        // check password and confirm password
-        if (req.body.password === req.body.confirm) {
-            // reset password using setPassword of passport-local-mongoose
-            user.setPassword(req.body.password, err => {
-                if (err) throw err;
-            user.resetPasswordToken = null;
-            user.resetPasswordExpires = null;
+            if (!user) {
+                req.flash("error", "Password reset token is invalid or has expired.");
+                return res.redirect("/password_reset");
+            }
+            // check password and confirm password
+            if (req.body.password === req.body.confirm) {
+                // reset password using setPassword of passport-local-mongoose
+                    user.setPassword(req.body.password, err => {
+                        if (err) throw err;
+                    user.resetPasswordToken = null;
+                    user.resetPasswordExpires = null;
 
-            user.save(err => {
-                if (err) throw err;
-            req.logIn(user, err => {
-                done(err, user);
-        });
-        });
-        });
-        } else {
-            req.flash("error", "Passwords do not match");
-            return res.redirect("back");
-        }
-    });
+                    user.save(err => {
+                        if (err) throw err;
+                        req.logIn(user, err => {
+                            done(err, user);
+                        });
+                    });
+                });
+            } else {
+                req.flash("error", "Passwords do not match");
+                return res.redirect("back");
+                }
+            });
     },
     function(user, done) {
         let smtpTransport = nodemailer.createTransport({
@@ -120,22 +120,22 @@ router.post("/reset/:token", (req, res) => {
         let mailOptions = {
             from: "dookwhipsers@gmail.com",
             to: user.email,
-            subject: "Your YelpCamp Password has been changed",
+            subject: "Your grateful wall Password has been changed",
             text: "Hi " + user.firstName + ",\n\n" +
             "This is a confirmation that the password for your account " + user.email + "  has just been changed.\n\n" +
             "Best,\n"+
-            "The YelpCamp Team\n"
+            "The Grateful Wall Team\n"
         };
         smtpTransport.sendMail(mailOptions, err => {
             if (err) throw err;
         req.flash("success", "Your password has been changed.");
         done(err);
-    });
+        });
     },
-], err => {
-    if (err) throw err;
-res.redirect("login");
-});
+    ], err => {
+        if (err) throw err;
+        res.redirect("login");
+        });
 });
 
 module.exports = router;
