9924f1d69c6f9e888935c8c27b50f315afb0040b
Rompasaurus <michael.e.mchenry@gmail.com>
2024-01-11 23:30:50 +0100

    undoing it didnt work

--- app.js
+++ app.js
@@ -62,14 +62,6 @@ app.use(function(req,res,next) {
     res.locals.currentUser=req.user;
     next();
 })
-app.use(function(request, response, next) {
-
-    if (process.env.NODE_ENV != 'development' && !request.secure) {
-       return response.redirect("https://" + request.headers.host + request.url);
-    }
-
-    next();
-})
 
 //Initialize routes
 app.use(auth);
